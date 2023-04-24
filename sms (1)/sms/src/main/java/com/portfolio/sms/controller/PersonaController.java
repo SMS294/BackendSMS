79397d049cbdda92b2fdb21d51b1cd13dcc50869
SMS294 <salinasandramariana@gmail.com>
2023-04-24 09:02:03 -0300

    java 17 arreglos anda la authenticacion del login jwt

--- sms (1)/sms/src/main/java/com/portfolio/sms/controller/PersonaController.java
+++ sms (1)/sms/src/main/java/com/portfolio/sms/controller/PersonaController.java
@@ -10,28 +10,28 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/personas")
-@CrossOrigin
+@CrossOrigin (origins = "*")
 public class PersonaController {
     @Autowired
     IPersonaService iPersonaService;
-    @GetMapping("/personas/traer")
+    @GetMapping("/traer")
     public List<Persona> getPersona(){
         return iPersonaService.getPersona();}
 
     @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/personas/crear")
+    @PostMapping("/crear")
     public String createPersona(@RequestBody Persona persona){
         iPersonaService.savePersona(persona);
         return "la persona fue creada correctamente";
     }
     @PreAuthorize( "hasRole('ADMIN' )")
-    @DeleteMapping("/personas/borrar/{id}")
+    @DeleteMapping("/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         iPersonaService.deletePersona(id);
         return "la persona fue eliminada correctamente";
     }
     @PreAuthorize( "hasRole('ADMIN' )")
-    @PutMapping("/personas/editar/{id}")
+    @PutMapping("/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam ("nombre") String nuevoNombre,
                                @RequestParam ("apellido") String nuevoApellido,
