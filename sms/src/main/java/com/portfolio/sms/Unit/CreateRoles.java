0cfb38b2b390a7867c756e6d8181b8eb0b8806c9
SMS294 <salinasandramariana@gmail.com>
2023-06-12 17:48:21 -0300

    Revert "volume en el dockerfile"
    This reverts commit 10fa6b3e1aa026b9d3e1bf602df221b7319286c6.

--- sms/src/main/java/com/portfolio/sms/Unit/CreateRoles.java
+++ sms/src/main/java/com/portfolio/sms/Unit/CreateRoles.java
@@ -1,24 +1,24 @@
-package com.portfolio.sms.Unit;
+//package com.portfolio.sms.Unit;
 
-import com.portfolio.sms.security.entity.Rol;
-import com.portfolio.sms.security.enums.RolNombre;
-import com.portfolio.sms.security.service.RolService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+//import com.portfolio.sms.security.entity.Rol;
+//import com.portfolio.sms.security.enums.RolNombre;
+//import com.portfolio.sms.security.service.RolService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.stereotype.Component;
 
-@Component
-public class CreateRoles implements CommandLineRunner {
+//@Component
+//public class CreateRoles implements CommandLineRunner {
 
-    @Autowired
-    RolService  rolService;
-    @Override
-   public void run(String... args) throws Exception {
-        Rol rolAdmin = new Rol( RolNombre.ROLE_ADMIN );
-        Rol rolUser = new Rol( RolNombre.ROLE_USER );
-       rolService.save( rolAdmin );
-       rolService.save( rolUser );
+   // @Autowired
+   // RolService// rolService;
+   // @Override
+   //public void run(String... args) throws Exception {
+       // Rol rolAdmin = new Rol( RolNombre.ROLE_ADMIN );
+       // Rol rolUser = new Rol( RolNombre.ROLE_USER );
+       //rolService.save( rolAdmin );
+      // rolService.save( rolUser );
 
-   }
-}
+  // }//
+//}
 
