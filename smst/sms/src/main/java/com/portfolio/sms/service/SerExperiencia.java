b1476c3d9ac40045269826bc4f7b9ace8e64ce7f
SMS294 <salinasandramariana@gmail.com>
2023-06-30 13:22:02 -0300

    arreglos

--- smst/sms/src/main/java/com/portfolio/sms/service/SerExperiencia.java
+++ smst/sms/src/main/java/com/portfolio/sms/service/SerExperiencia.java
@@ -8,9 +8,9 @@ import org.springframework.stereotype.Service;
 import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
+
 @Service
 @Transactional
-
 public class SerExperiencia {
     @Autowired
     RepExperiencia repExperiencia;
